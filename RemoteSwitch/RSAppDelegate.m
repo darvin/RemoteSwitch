54fef859836271d225a331e6514cb98064a4fc1c
Sergey Klimov <dcdarv@gmail.com>
2011-12-10 22:15:16 -0600

    small changes

--- RemoteSwitch/RSAppDelegate.m
+++ RemoteSwitch/RSAppDelegate.m
@@ -19,7 +19,6 @@
 }
 
 -(void) setRemoteEnabled:(BOOL)enabled {
-    NSLog(@"%@", remoteMenuItem);
     if ([[HIDRemote sharedHIDRemote] isStarted])
         [[HIDRemote sharedHIDRemote] stopRemoteControl];
 
@@ -82,120 +81,93 @@
 }
 
 
-- (BOOL)hidRemote:(HIDRemote *)hidRemote lendExclusiveLockToApplicationWithInfo:(NSDictionary *)applicationInfo
-{
-	NSNumber *remoteModeNumber;
-    
-	if ((remoteModeNumber = [applicationInfo objectForKey:kHIDRemoteDNStatusModeKey]) != nil)
-	{
-		switch ((HIDRemoteMode)[remoteModeNumber intValue])
-		{
-                // Lend exclusive lock to all applications operating in shared or
-                // exclusive-auto mode
-			case kHIDRemoteModeShared:
-			case kHIDRemoteModeExclusiveAuto:
-				return (YES);
-                break;
-		}
-	}
-	
-	// Don't lend the lock to applications operating in other modes
-	return (NO);
-}
-
-
 
 #pragma mark -- Handle remote control events --
 - (void)hidRemote:(HIDRemote *)hidRemote eventWithButton:(HIDRemoteButtonCode)buttonCode isPressed:(BOOL)isPressed fromHardwareWithAttributes:(NSMutableDictionary *)attributes
 {
     
-    
-    
-	NSString *buttonName = nil;
-    
-	switch (buttonCode)
-	{
-		case kHIDRemoteButtonCodeUp:
-			buttonName = @"Up";
-            break;
-            
-		case kHIDRemoteButtonCodeUpHold:
-			buttonName = @"Up (hold)";
-            break;
-            
-		case kHIDRemoteButtonCodeDown:
-			buttonName = @"Down";
-            break;
-            
-		case kHIDRemoteButtonCodeDownHold:
-			buttonName = @"Down (hold)";
-            break;
-            
-		case kHIDRemoteButtonCodeLeft:
-			buttonName = @"Left";
-            break;
-            
-		case kHIDRemoteButtonCodeLeftHold:
-			buttonName = @"Left (hold)";
-            break;
-            
-		case kHIDRemoteButtonCodeRight:
-			buttonName = @"Right";
-            break;
-            
-		case kHIDRemoteButtonCodeRightHold:
-			buttonName = @"Right (hold)";
-            break;
-            
-		case kHIDRemoteButtonCodeCenter:
-			buttonName = @"Center";
-            break;
-            
-		case kHIDRemoteButtonCodeCenterHold:
-			buttonName = @"Center (hold)";
-            break;
-            
-		case kHIDRemoteButtonCodeMenu:
-			buttonName = @"Menu";
-            break;
-            
-		case kHIDRemoteButtonCodeMenuHold:
-			buttonName = @"Menu (hold)";
-            break;
-            
-		case kHIDRemoteButtonCodePlay:
-			buttonName = @"Play (Alu Remote!)";
-            break;
-            
-		case kHIDRemoteButtonCodePlayHold:
-			buttonName = @"Play (Alu Remote!) (hold)";
-            break;
-	}
+//    
+//    
+//	NSString *buttonName = nil;
+//    
+//	switch (buttonCode)
+//	{
+//		case kHIDRemoteButtonCodeUp:
+//			buttonName = @"Up";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeUpHold:
+//			buttonName = @"Up (hold)";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeDown:
+//			buttonName = @"Down";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeDownHold:
+//			buttonName = @"Down (hold)";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeLeft:
+//			buttonName = @"Left";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeLeftHold:
+//			buttonName = @"Left (hold)";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeRight:
+//			buttonName = @"Right";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeRightHold:
+//			buttonName = @"Right (hold)";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeCenter:
+//			buttonName = @"Center";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeCenterHold:
+//			buttonName = @"Center (hold)";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeMenu:
+//			buttonName = @"Menu";
+//            break;
+//            
+//		case kHIDRemoteButtonCodeMenuHold:
+//			buttonName = @"Menu (hold)";
+//            break;
+//            
+//		case kHIDRemoteButtonCodePlay:
+//			buttonName = @"Play (Alu Remote!)";
+//            break;
+//            
+//		case kHIDRemoteButtonCodePlayHold:
+//			buttonName = @"Play (Alu Remote!) (hold)";
+//            break;
+//	}
 	
 	if (isPressed)
 	{
-		NSLog(@"Button \"%@\" was pressed", buttonName);
 	}
 	else
 	{
-		NSLog(@"Button \"%@\" was released", buttonName);
         if (lastTimeButtonPressed  < (CFAbsoluteTimeGetCurrent() - kSecondsBetweenButtons)) {
             [pressedButtons removeAllObjects];
-            NSLog(@"toolong!");
         }
         
         
         [pressedButtons addObject: [NSNumber numberWithInt:buttonCode]];
         
-        NSLog(@"%@", pressedButtons);
         
         if ([pressedButtons count]>=[buttonSequence count]) {
             NSArray * slice = [pressedButtons subarrayWithRange:
                            (NSRange){[pressedButtons count]-[buttonSequence count], [buttonSequence count]}];
-            NSLog(@"slice %@ \n pressedButtons %@ \n sequence %@", slice, pressedButtons, buttonSequence);
             if ([slice isEqualToArray:buttonSequence]) {
                 [pressedButtons removeAllObjects];
-                [self performSelector:@selector(toggleRemote:)withObject:self afterDelay:1.0];
+                [self performSelector:@selector(toggleRemote:)withObject:self afterDelay:0.01];
             }
         }
         
